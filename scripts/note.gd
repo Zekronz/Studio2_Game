677d14e36b7af9578b63d455d22c39563e0c7502
Zekronz <zekronpvp@gmail.com>
2026-04-10 00:39:41 +0200

    Separate hold body

--- scripts/note.gd
+++ scripts/note.gd
@@ -1,5 +1,24 @@
 extends Node3D
 
+const NOTE_WIDTH : float = 0.8
+const NOTE_HEIGHT : float = 0.07
+const NOTE_BASE_LENGTH : float = 0.4
+
+const HOLD_WIDTH : float = 0.7
+const HOLD_HEIGHT : float = NOTE_HEIGHT - 0.06
+
 var time : float = 0
 var length : float = 0
 var is_hold : bool = false
+
+func init(n_time : float, n_length : float, n_is_hold : bool, hold_length) -> void:
+	time = n_time
+	length = n_length
+	is_hold = n_is_hold
+	
+	$Main.scale = Vector3(NOTE_WIDTH, NOTE_HEIGHT, NOTE_BASE_LENGTH)
+	
+	if is_hold:
+		$Hold.visible = true
+		$Hold.scale = Vector3(HOLD_WIDTH, HOLD_HEIGHT, hold_length)
+	
--- scripts/note_handler.gd
+++ scripts/note_handler.gd
@@ -1,10 +1,6 @@
 extends Node
 
-const SCROLL_SPEED : float = 10.0
-
-const NOTE_WIDTH : float = 0.8
-const NOTE_HEIGHT : float = 0.07
-const NOTE_BASE_LENGTH : float = 0.4
+const SCROLL_SPEED : float = 15.0
 
 @onready var audio_handler : Node = $AudioStreamPlayer
 @onready var playfield : Node3D = $Playfield
@@ -14,7 +10,8 @@ var note_scene : Resource = preload("res://scenes/note.tscn")
 func _ready() -> void:
 	assert(note_group != null)
 	
-	var map = MapParser.load_map("res://maps/Testify/void (Mournfinale) feat. Hoshikuma Minami - Testify (Kyousuke-) [Fatalism].osu")
+	#var map = MapParser.load_map("res://maps/Testify/void (Mournfinale) feat. Hoshikuma Minami - Testify (Kyousuke-) [Fatalism].osu")
+	var map = MapParser.load_map("res://maps/Finixe/Silentroom - Finixe (shuniki) [ShuChan!!].osu")
 	
 	playfield.set_key_count(map["key_count"])
 	
@@ -35,7 +32,7 @@ func spawn_note_at(column : int, start_time : float, end_time : float = -1) -> v
 	assert(column >= 0 && column < playfield.key_count)
 	
 	var note_pos = get_note_pos(start_time, audio_handler.get_playback_position())
-	var note_length = NOTE_BASE_LENGTH
+	var hold_length = 0.0
 	
 	var is_hold = false
 	var time_length = 0.0
@@ -46,12 +43,9 @@ func spawn_note_at(column : int, start_time : float, end_time : float = -1) -> v
 		time_length = end_time - start_time
 		assert(time_length > 0.0)
 		
-		note_length = (time_length * SCROLL_SPEED)
+		hold_length = (time_length * SCROLL_SPEED)
 	
 	var note = note_scene.instantiate();
-	note.time = start_time
-	note.length = time_length
-	note.is_hold = is_hold
 	note.position = (playfield.get_column_center(column) * Vector3.LEFT) + (note_pos * Vector3.BACK)
-	note.scale = Vector3(NOTE_WIDTH, NOTE_HEIGHT, note_length)
+	note.init(start_time, time_length, is_hold, hold_length)
 	note_group.add_child(note)
